c7b6829a6a9fa777b30a47d669a49e6c21e5c914
Girish Pai <girishpai86@gmail.com>
2017-07-17 15:47:54 -0700

    feat : Complete implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,8 @@
 #include "PID.h"
+#include<vector>
+#include<numeric>
+#include "json.hpp"
+using json = nlohmann::json;
 
 using namespace std;
 
@@ -6,16 +10,30 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+  prev_cte = 0;
+  int_cte = 0;
+}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  this->p_error = this->Kp * cte;
+
+  this->int_cte += cte;
+  double diff_cte = cte - this->prev_cte;
+  this->prev_cte = cte;
+  this->d_error = this->Kd * diff_cte;
+  this->i_error = this->Ki * this->int_cte;
 }
 
 double PID::TotalError() {
+  return p_error + d_error + i_error;
 }
 
